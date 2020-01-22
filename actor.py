7e4a8f6155c61bf115e07bcc70471478b4fdf4e6
Aslak Hollund <aslakhol@gmail.com>
2020-01-22 16:36:20 +0100

    begin refactor

--- actor.py
+++ actor.py
@@ -9,30 +9,35 @@ class Actor:
         self.discountFactor = settings["discountFactor"]
         self.epsilon = settings["epsilon"]
         self.epsilonDecayRate = settings["epsilonDecayRate"]
-        self.eligibilityMap ={} 
-        self.policy = {} # (s,a) -> z where z is how desirable the action is in the current state
+        self.eligibilityMap = {}
+        self.policy = (
+            {}
+        )  # (s,a) -> z where z is how desirable the action is in the current state
 
     def updateEligibility(self, state, action, isCurrentState=False):
         if isCurrentState:
             self.eligibilityMap.get(state, {})[action] = 1
         else:
-            self.eligibilityMap.get(state,{})[action] = (
+            self.eligibilityMap.get(state, {})[action] = (
                 self.discountFactor
                 * self.eligibilityDecayRate
-                * self.eligibilityMap.get(state, {}).get(action, random.uniform(0,1))
+                * self.eligibilityMap.get(state, {}).get(action, random.uniform(0, 1))
             )
 
     def chooseAction(self, state, legalMoves):
-        probability = random.randint(0, 100) / 100
+        probability = random.uniform(0, 1)
         self.epsilon = self.epsilon * self.epsilonDecayRate
         if self.epsilon > probability:
             return random.choice(legalMoves)
         else:
-            self.policy[state] = self.policy.get(state, {action: random.uniform(0,1) for action in legalMoves})
+            self.policy[state] = self.policy.get(
+                state, {action: random.uniform(0, 1) for action in legalMoves}
+            )
             return max(self.policy[state], key=self.policy[state].get)
 
     def updatePolicy(self, state, action, TDerror):
-        self.policy.get(state, {})[action] = (
-            self.policy.get(state, {}).get(action, random.uniform(0,1))
-            + self.learningRate * TDerror * self.eligibilityMap.get(state, {}).get(action, random.uniform(0,1))
+        self.policy.get(state, {})[action] = self.policy.get(state, {}).get(
+            action, random.uniform(0, 1)
+        ) + self.learningRate * TDerror * self.eligibilityMap.get(state, {}).get(
+            action, random.uniform(0, 1)
         )
--- agent.py
+++ agent.py
@@ -7,63 +7,69 @@ from matplotlib import pyplot as plt
 from drawer import Drawer
 import numpy as np
 
+
 class Agent:
     def __init__(self):
-        #states, SAP = generateAllSAP(settings["size"], settings["boardType"])
+        # states, SAP = generateAllSAP(settings["size"], settings["boardType"])
         self.actor = Actor()
         self.critic = Critic()
         self.drawer = Drawer()
 
     def runEpisodes(self, numberOfEpisodes):
         pegsLeft = []
-        for _ in range(numberOfEpisodes):
-            enviroment = Game()  # new game ish don't know the interface excactly
-            state = (
-                enviroment.board.getState()
-            )  # the board represented in a bitstring maybe initalize as None
-            action = None  # No action should be done initially.
-            SAPpairs = []
-            while not enviroment.isEndState():
-                newState, reinforcement = enviroment.board.move(action)
-                legalMoves = enviroment.board.allLegalMoves()
-                if enviroment.isEndState():
-                    newAction = action
-                else:
-                    newAction = self.actor.chooseAction(
-                        newState, legalMoves
-                    )  # The article about reinforcement learning, just states that the actor chooses an action, but i think it should know which actions are legal.
-                self.actor.updateEligibility(
-                    newState, newAction, isCurrentState=True
-                )  # This should update the eligibility of the SAP to 1, but that will be handled in the function
-                TDError = self.critic.getTDError(state, newState, reinforcement)
-                self.critic.updateEligibility(
-                    newState, isCurrentState=True
-                )  # Should be updated to 1
-                if action:
-                    SAPpairs.append((state, action))
+        for episodeNumber in range(numberOfEpisodes):
+            self.runEpisode(episodeNumber, pegsLeft)
+        self.displayResults(pegsLeft)
 
-                if _ == 500:
-                    self.drawer.draw(enviroment.board.board)
-
-                for SAP in SAPpairs:
-                    s, a = SAP
-                    self.critic.updateValueFunction(
-                        s, TDError
-                    )  # updates the entry for s in the valuefunction
-                    self.critic.updateEligibility(s)  # should be Y * gamma * e(s)
-                    self.actor.updatePolicy(
-                        s, a, TDError
-                    )  # update the entry for the sap in the actor
-                    self.actor.updateEligibility(
-                        s, a
-                    )  # update the eligibility y * gamma * e(s,a)
-                state, action = newState, newAction
-            if reinforcement==3000:
-                pegsLeft.append(24)
+    def runEpisode(self, episodeNumber, pegsLeft):
+        enviroment = Game()  # new game ish don't know the interface excactly
+        state = (
+            enviroment.board.getState()
+        )  # the board represented in a bitstring maybe initalize as None
+        action = None  # No action should be done initially.
+        SAPpairs = []
+        while not enviroment.isEndState():
+            newState, reinforcement = enviroment.board.move(action)
+            legalMoves = enviroment.board.allLegalMoves()
+            if enviroment.isEndState():
+                newAction = action
             else:
-                pegsLeft.append(reinforcement)
-        
-        a = np.convolve(pegsLeft, np.ones((100,))/100, mode='valid')
+                newAction = self.actor.chooseAction(
+                    newState, legalMoves
+                )  # The article about reinforcement learning, just states that the actor chooses an action, but i think it should know which actions are legal.
+            self.actor.updateEligibility(
+                newState, newAction, isCurrentState=True
+            )  # This should update the eligibility of the SAP to 1, but that will be handled in the function
+            TDError = self.critic.getTDError(state, newState, reinforcement)
+            self.critic.updateEligibility(
+                newState, isCurrentState=True
+            )  # Should be updated to 1
+            if action:
+                SAPpairs.append((state, action))
+
+            if pegsLeft == 500:
+                self.drawer.draw(enviroment.board.board)
+
+            for SAP in SAPpairs:
+                s, a = SAP
+                self.critic.updateValueFunction(
+                    s, TDError
+                )  # updates the entry for s in the valuefunction
+                self.critic.updateEligibility(s)  # should be Y * gamma * e(s)
+                self.actor.updatePolicy(
+                    s, a, TDError
+                )  # update the entry for the sap in the actor
+                self.actor.updateEligibility(
+                    s, a
+                )  # update the eligibility y * gamma * e(s,a)
+            state, action = newState, newAction
+        if reinforcement == 3000:
+            pegsLeft.append(24)
+        else:
+            pegsLeft.append(reinforcement)
+
+    def displayResults(self, pegsLeft):
+        a = np.convolve(pegsLeft, np.ones((100,)) / 100, mode="valid")
         plt.plot(a)
         plt.show()
         print(pegsLeft)
