b18328f12b300ef279f91a87f07e1ed9a7515bbe
Aslak Hollund <aslakhol@gmail.com>
2020-01-22 21:29:03 +0100

    commentgeddon

--- agent.py
+++ agent.py
@@ -29,8 +29,7 @@ class Agent:
     def initalizeEpisode(self):
         board = Board(settings["size"], settings["boardType"], settings["state"])
         state = board.bitString
-        # the board represented in a bitstring maybe initalize as None
-        action = None  # No action should be done initially.
+        action = None
         SAPpairs = []
         return board, state, action, SAPpairs
 
@@ -39,41 +38,27 @@ class Agent:
         if board.isInEndState():
             newAction = action
         else:
-            newAction = self.actor.chooseAction(
-                newState, legalMoves
-            )  # The article about reinforcement learning, just states that the actor chooses an action, but i think it should know which actions are legal.
+            newAction = self.actor.chooseAction(newState, legalMoves)
 
         return newAction
 
     def runEpisode(self, episodeNumber, pegsLeft):
-        # Initialize s and a
         board, state, action, SAPpairs = self.initalizeEpisode()
         while not board.isInEndState():
-            # Do action a from state s moving system to newState and receiving reinforcement r.
             newState, reinforcement = self.takeMove(action, board)
             newAction = self.chooseNextAction(board, action, newState)
-            self.actor.updateEligibility(
-                newState, newAction, isCurrentState=True
-            )  # This should update the eligibility of the SAP to 1, but that will be handled in the function
+            self.actor.updateEligibility(newState, newAction, isCurrentState=True)
             TDError = self.critic.getTDError(state, newState, reinforcement)
-            self.critic.updateEligibility(
-                newState, isCurrentState=True
-            )  # Should be updated to 1
+            self.critic.updateEligibility(newState, isCurrentState=True)
             if action:
                 SAPpairs.append((state, action))
 
             for SAP in SAPpairs:
                 s, a = SAP
-                self.critic.updateValueFunction(
-                    s, TDError
-                )  # updates the entry for s in the valuefunction
-                self.critic.updateEligibility(s)  # should be Y * gamma * e(s)
-                self.actor.updatePolicy(
-                    s, a, TDError
-                )  # update the entry for the sap in the actor
-                self.actor.updateEligibility(
-                    s, a
-                )  # update the eligibility y * gamma * e(s,a)
+                self.critic.updateValueFunction(s, TDError)
+                self.critic.updateEligibility(s)
+                self.actor.updatePolicy(s, a, TDError)
+                self.actor.updateEligibility(s, a)
             state, action = newState, newAction
         if reinforcement == 3000:
             pegsLeft.append(24)
